6d4f53014dd46185e14f6f1878c71edce406f37d
Yashaswi4701 <yashaswisingh47@gmail.com>
2024-04-16 15:58:41 +0530

    added js files in utils to provide wrapper

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-import { MongoGridFSChunkError } from "mongodb";
+
 
 const connectDB = async () => {
     try {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-// require('dotenv').config({path : './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path : './env'
-})
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed : ", err);
+  });
